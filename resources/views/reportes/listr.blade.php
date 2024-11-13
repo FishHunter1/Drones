5515280667cf65aa8a9a65845bddfd21512b8d4f
FishHunter1 <afbm2700@gmail.com>
2024-11-12 22:40:40 -0500

    Arreglo en los colores

--- resources/views/reportes/listr.blade.php
+++ resources/views/reportes/listr.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Lista-Camiones')
+@section('title', 'Lista-Reportes')
 
 @section('content')
 <body data-spy="scroll" data-target="#ftco-navbar" data-offset="200">
@@ -47,10 +47,10 @@
                 <li class="dropdown">
                   <a href="{{route('liste')}}" class="nav-link"><i class="fa fa-user"></i> <span>Empleados</span></a>
                 </li>
-                <li class="dropdown active">
+                <li class="dropdown">
                     <a href="{{route('listc')}}" class="nav-link" data-tooggle="dropdown"><i class="fa fa-truck"></i> <span>Camiones</span></a>
                 </li>
-                <li class="dropdown">
+                <li class="dropdown active">
                     <a href="{{route('reportesm')}}" class="nav-link" data-tooggle="dropdown"><i class="far fa-file-alt"></i> <span>Reportes</span></a>
                 </li>
                 <li class="dropdown">
@@ -67,12 +67,12 @@
         <div class="main-content" style="flex-grow: 1;">
             <section class="section">
                 <div class="section-header">
-                    <h1 class="text-primary">Lista de Camiones</h1>
+                    <h1 class="text-primary">Lista de Reportes</h1>
                 </div>
                 <div class="section-body">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Camiones Registrados</h4>
+                            <h4 class="card-title">Reportes Registrados</h4>
                         </div>
                         <div class="card-body">
                             <table class="table table-striped table-bordered rounded-lg shadow-lg">
@@ -92,7 +92,10 @@
                             </table>
                         </div>
                         <div class="card-footer text-right">
-                            <a href="{{ route('formc') }}" class="btn btn-success"><i class="fas fa-plus"></i> Agregar Camión</a>
+                            <a href="{{ route('reportesm') }}" class="btn btn-success"><i class="fas fa-plus"></i> Agregar Reporte Mantenimiento</a>
+                        </div>
+                        <div class="card-footer text-right">
+                            <a href="{{ route('reportesi') }}" class="btn btn-success"><i class="fas fa-plus"></i> Agregar Reporte Incidente</a>
                         </div>
                     </div>
                 </div>
